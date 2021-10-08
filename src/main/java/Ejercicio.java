a2460e7e248262ac19b58974ca45638d71539700
beabp <beatriz.borrego1@educa.madrid.org>
2021-10-08 10:39:09 +0200

    Edad y estudios

---
+++ src/main/java/Ejercicio.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ejercicio {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Introduce tu edad:");
+        int age=sc.nextInt();
+
+        System.out.println("Introduce tu nivel de estudios:");
+        String studies= sc.nextLine();
+
+    }
+}
